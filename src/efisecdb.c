af1f034300a54ac94ac6b607977f6f3b4a86fb6f
Peter Jones <pjones@redhat.com>
2026-03-26 10:29:24 -0400

    efisecdb: hide SHA-1 support
    While it's nice to support SHA-1 so we can e.g. build test databases
    that look like that one insane vendor's 'db', there's no reason to
    advertise this and encourage people to do it.
    
    This patch makes it so "efisecdb -t help" doesn't show that we support
    SHA-1.
    
    Signed-off-by: Peter Jones <pjones@redhat.com>

--- src/efisecdb.c
+++ src/efisecdb.c
@@ -33,6 +33,7 @@ struct hash_param {
 	efi_secdb_type_t algorithm;
 	ssize_t size;
 	bool def;
+	bool hidden;
 };
 
 static struct hash_param hash_params[] = {
@@ -40,21 +41,25 @@ static struct hash_param hash_params[] = {
 	 .algorithm = EFI_SECDB_TYPE_SHA512,
 	 .size = 64,
 	 .def = false,
+	 .hidden = false,
 	},
 	{.name = "sha384",
 	 .algorithm = EFI_SECDB_TYPE_SHA384,
 	 .size = 48,
 	 .def = false,
+	 .hidden = false,
 	},
 	{.name = "sha256",
 	 .algorithm = EFI_SECDB_TYPE_SHA256,
 	 .size = 32,
 	 .def = true,
+	 .hidden = false,
 	},
 	{.name = "sha1",
 	 .algorithm = EFI_SECDB_TYPE_SHA1,
 	 .size = 20,
 	 .def = false,
+	 .hidden = true,
 	},
 };
 static int n_hash_params = sizeof(hash_params) / sizeof(hash_params[0]);
@@ -81,9 +86,11 @@ set_hash_parameters(char *name, int *hash_number)
 
 	fprintf(out, "Supported hashes:");
 	for (int i = 0; i < n_hash_params; i++) {
-		fprintf(out, " %s", hash_params[i].name);
 		if (hash_params[i].def)
 			def = i;
+		if (hash_params[i].hidden)
+			continue;
+		fprintf(out, " %s", hash_params[i].name);
 	}
 	fprintf(out, "\n");
 	if (def >= 0)
